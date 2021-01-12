65004db2875a16b832eb57c9eae37517664f558c
Josh Clark <josh.clark@Joshs-MacBook-Pro.local>
2021-01-12 14:07:26 -0500

    Added an app secret.

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return os.environ.get('APP_MESSAGE', 'Something is wrong, this is the backup message.')
+    text = "{}<br><br>Here's my secret: {}".format(
+        os.environ.get('APP_MESSAGE', 'This is the backup message. Something is wrong!'),
+        os.environ.get('APP_SECRET')
+    )
+    return text
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
